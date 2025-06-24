17fd07f9c295c2df52ec5f896075b49797ec4239
amjadglory <alamjad211096@gmail.com>
2025-06-25 01:38:43 +0300

    making a real time validation in the sign up page

--- js/signUp.js
+++ js/signUp.js
@@ -2,7 +2,14 @@
 var nameInput = document.getElementById("name");
 var emailInput = document.getElementById("email");
 var passInput = document.getElementById("pass");
-var incorrectWarning = document.getElementById("incorrect");
+var nameInvalidMsg = document.getElementById("nameInvalidMsg");
+var mailInvalidMsg = document.getElementById("mailInvalidMsg");
+var passInvalidMsg = document.getElementById("passInvalidMsg");
+var nameValidMsg = document.getElementById("nameValidMsg");
+var mailValidMsg = document.getElementById("mailValidMsg");
+var passValidMsg = document.getElementById("passValidMsg");
+
+// var incorrectWarning = document.getElementById("incorrect");
 var existsWarning = document.getElementById("exists");
 var successWarning = document.getElementById("success");
 var signUpBtn = document.getElementById("signUpBtn");
@@ -10,7 +17,7 @@ var signInLink = document.getElementById("signIn");
 
 //! vars
 var users = [];
-var nameRegex = /^[A-Z][A-Za-z ]{2,25}$/;
+var passRegex = /^[A-Z][A-Za-z ]{2,25}$/;
 var mailRegex = /^((?!\.)[\w-_.]*[^.])(@\w+)(\.\w+(\.\w+)?[^.\W])$/gim;
 var passRegex = /^[0-9]{3,9}[a-z]{0,1}$/;
 
@@ -27,59 +34,80 @@ function addUser() {
     email: emailInput.value,
     pass: passInput.value,
   };
-// هنا بعرف اذا كان الايميل مستخدم قبل كدا ولا لا , لو الايميل مستخدم بريتيرن فوولس و انهى الفنكتشن
+  // هنا بعرف اذا كان الايميل مستخدم قبل كدا ولا لا , لو الايميل مستخدم بريتيرن فوولس و انهى الفنكتشن
   for (var i = 0; i < users.length; i++) {
     if (user.email === users[i].email) {
-      incorrectWarning.classList.add("d-none");
       existsWarning.classList.remove("d-none");
       return false;
     }
   }
-// هنا الفاليديتشن لو فاليد بنبوشش اليوزر الجديد فالارراى و اللوكال ستوردج و نريتيرنن ترو
-//  و لو خطاء بنظهر له رساله توضح فيها كل الشروط و تريترن فولس
+  // هنا الفاليديتشن لو فاليد بنبوشش اليوزر الجديد فالارراى و اللوكال ستوردج و نريتيرنن ترو
+  //  و لو خطاء بنظهر له رساله توضح فيها كل الشروط و تريترن فولس
   if (
-    nameRegex.test(user.name) &&
-    mailRegex.test(user.email) &&
-    passRegex.test(user.pass)
+    validation(nameInput, "nameValidMsg", "nameInvalidMsg") &&
+    validation(emailInput, "mailValidMsg", "mailInvalidMsg") &&
+    validation(passInput, "passValidMsg", "passInvalidMsg")
   ) {
     users.push(user);
     localStorage.setItem("users", JSON.stringify(users));
     existsWarning.classList.add("d-none");
-    incorrectWarning.classList.add("d-none");
     successWarning.classList.remove("d-none");
     return true;
   } else if (
-    !nameRegex.test(user.name) ||
-    !mailRegex.test(user.email) ||
-    !passRegex.test(user.pass)
+    !validation(nameInput, "nameValidMsg", "nameInvalidMsg") ||
+    !validation(emailInput, "mailValidMsg", "mailInvalidMsg") ||
+    !validation(passInput, "passValidMsg", "passInvalidMsg")
   ) {
     existsWarning.classList.add("d-none");
-    incorrectWarning.classList.remove("d-none");
+
+    console.log("lol");
+    return false;
+  }
+}
+
+function validation(input, validMsgP, invalidMsgP) {
+  var inputValue = input.value;
+  var regex = {
+    name: /^[A-Z][A-Za-z ]{2,25}$/,
+    email: /^((?!\.)[\w-_.]*[^.])(@\w+)(\.\w+(\.\w+)?[^.\W])$/gim,
+    pass: /^[0-9]{3,9}[a-z]{0,1}$/,
+  };
+
+  var invalidMsg = document.getElementById(invalidMsgP);
+  var validMsg = document.getElementById(validMsgP);
+  if (regex[input.id].test(inputValue)) {
+    invalidMsg.classList.add("d-none");
+    validMsg.classList.remove("d-none");
+    return true;
+  } else {
+    invalidMsg.classList.remove("d-none");
+    validMsg.classList.add("d-none");
     return false;
   }
 }
 
 //! Events
 signUpBtn.addEventListener("click", function () {
-  if (addUser()) { // لو الاادد يوزر رجعت ترو
+  if (addUser()) {
+    // لو الاادد يوزر رجعت ترو
     addUser(); // شغل فنكتشن الااد يوزر
-    signUpBtn.setAttribute("href", "./index.html"); // روح ع صفحه البروفايل 
+    signUpBtn.setAttribute("href", "./index.html"); // روح ع صفحه البروفايل
     existsWarning.classList.add("d-none");
     incorrectWarning.classList.add("d-none");
     successWarning.classList.remove("d-none");
+  } else {
+    console.log("rr");
   }
 });
 
 // /* ============================================= */
 
 // الحل بتاعى الاول و اللى هو غلط بس مش مجمع اوى السبب فا عاوز اعمل ترييس للكود معاك لان الهاره دى عندى مش قد كدا
-// اللى انا واخد بالى منه انى معملتش تقسيم للفنكتشن بتاعتى زى مانا عامل فوق بمساعده جروك 
+// اللى انا واخد بالى منه انى معملتش تقسيم للفنكتشن بتاعتى زى مانا عامل فوق بمساعده جروك
 // المفروض اقسم الفنكتشن مش احط كل الفنكتشن ع بعض كدا فى اف كوندتشن واحده جوا لووب واحده
-// لا لازم الف الاول باللوب ع اليوزرز و اشوف الايميل دا مستخدم ولا لا 
+// لا لازم الف الاول باللوب ع اليوزرز و اشوف الايميل دا مستخدم ولا لا
 // لو المييل مستخدم قبل كدا بظهر رساله خطا و اريتيرن فوولس عشان خاطر متنقلش لصفحه اللوج ان و الايفينت يقف لما ادوس ساين اب
-// لو مش مستخدم قبل كدا الايفينت بيكول الفنكتشن عادى و بتريترن ترو للايفينت فا و بروح لصفحه اللوج ان 
-
-
+// لو مش مستخدم قبل كدا الايفينت بيكول الفنكتشن عادى و بتريترن ترو للايفينت فا و بروح لصفحه اللوج ان
 
 // //! select elements
 // var nameInput = document.getElementById("name");
